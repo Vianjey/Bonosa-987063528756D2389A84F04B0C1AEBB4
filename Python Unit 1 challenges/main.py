ed13b716d8924c27bd59ffdd760183a89f1fb3bb
Vianjey <145749447+Vianjey@users.noreply.github.com>
2023-10-09 15:43:17 +0530

    Add files via upload

--- Python Unit 1 challenges/main.py
+++ Python Unit 1 challenges/main.py
@@ -1,12 +1,9 @@
-def isLeapYear(year):
-  if(year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    return True
-  else:
-    return False
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
-year = int(input("Enter a year : "))
-
-if isLeapYear(year):
-  print("{} is a leap year".format(year))
-else:
-  print("{} is not a leap year".format(year))
+number = int(input("Enter a value:"))
+result = factorial(number)
+print("The factorial of {} is {}".format(number,result))
